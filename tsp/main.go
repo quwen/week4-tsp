6eae96dc43c06ad8bc00978edd9c06e6a2ae4f4a
quwen <s30gd.3@mgmail.com>
2020-06-24 14:26:23 +0900

    Add printSolution

--- tsp/main.go
+++ tsp/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/csv"
+	"flag"
 	"log"
 	"os"
 	"strconv"
@@ -12,9 +14,10 @@ type City struct {
 	y float64
 }
 
-func readInput(num string) []City {
+//readInput is reads a csv file and returns a slice of the city's coordinates.
+func readInput(in string) []City {
 	//Path of the file to be read
-	path := "../input_" + num + ".csv"
+	path := "../input_" + in + ".csv"
 
 	cities := make([]City, 0)
 	//Load a file
@@ -34,4 +37,34 @@ func readInput(num string) []City {
 		cities = append(cities, City{x, y})
 	}
 	return cities
+}
+
+//printSolution writes the result to a csv file.
+func printSolution(in string, solution []string) {
+	path := "../solution_yours_" + in + ".csv"
+	//書き込み用に開く
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	for _, s := range solution {
+		if err := writer.Write(s); err != nil {
+			log.Fatalln("error writing record to csv:", err)
+		}
+	}
+}
+
+func main() {
+	var n = flag.String("input number", "0", "input number is 0-6")
+	flag.Parse()
+	cities := readInput(*n)
+
+	var solution []string
+
+	printSolution(*in, solution)
+
 }
