b96f204fe0c6d719eb984df64473838c5e91e8db
quwen <s30gd.3@mgmail.com>
2020-06-24 16:58:09 +0900

    Fix Error

--- tsp/main.go
+++ tsp/main.go
@@ -18,12 +18,79 @@ type City struct {
 	y float64
 }
 
+type Tour struct {
+	tourCities []City
+	distance   float64
+}
+
 func (a *City) DistanceTo(b City) float64 {
 	dx := float64(a.x - b.x)
 	dy := float64(a.y - b.y)
 
 	fd := math.Sqrt((dx * dx) + (dy * dy))
 	return fd
+}
+
+func (a *Tour) GetCity(tourPosition int) City {
+	return a.tourCities[tourPosition]
+}
+
+func (a *Tour) TourSize() int {
+	return len(a.tourCities)
+}
+
+func (a *Tour) TourDistance() float64 {
+	if a.distance == 0 {
+		td := float64(0)
+		for i := 0; i < a.TourSize(); i++ {
+			fromC := a.GetCity(i)
+			var destC City
+			if i+1 < a.TourSize() {
+				destC = a.GetCity(i + 1)
+			} else {
+				destC = a.GetCity(0)
+			}
+			td += fromC.DistanceTo(destC)
+		}
+		a.distance = td
+	}
+	return a.distance
+}
+
+func min(a []float64) float64 {
+	min := 0
+	for i, v := range a {
+		if v < a[min] {
+			min = i
+		}
+	}
+	return min
+}
+
+func solve(cities []City) int {
+	var solution []int
+	solution = append(solution, 0)
+
+	var dst [][]float64
+
+	for i := 1; i < len(cities); i++ {
+		for j := 1; j < len(cities); i++ {
+			if i == j;continue
+		dst[i-1] = append(dst[i-1], cities[i].DistanceTo(cities[j]))
+	}
+
+	var unvcities []City
+	for i := 1; i < len(cities); i++ {
+	unvcities = append(unvcities, cities[i])
+	}
+	city := 0
+	for i := 0; i < len(unvities); {
+		ncity := min(unvcities(dist[city]))
+	}
+
+
+
+
 }
 
 //readInput is reads a csv file and returns a slice of the city's coordinates.
@@ -53,7 +120,7 @@ func readInput(in string) []City {
 }
 
 //printSolution writes the result to a csv file.
-func printSolution(in string, solution []string) {
+func printSolution(in string, solution [][]string) {
 	path := "../solution_yours_" + in + ".csv"
 	//書き込み用に開く
 	file, err := os.Create(path)
@@ -64,8 +131,10 @@ func printSolution(in string, solution []string) {
 	defer file.Close()
 
 	writer := csv.NewWriter(file)
-	if err := writer.Write(solution); err != nil {
-		log.Fatalln("error writing record to csv:", err)
+	for _, s := range solution {
+		if err := writer.Write(s); err != nil {
+			log.Fatalln("error writing record to csv:", err)
+		}
 	}
 
 	writer.Flush()
@@ -75,17 +144,19 @@ func main() {
 	var in = flag.String("input", "0", "input number is 0-6")
 	flag.Parse()
 	cities := readInput(*in)
-	var solution []string
+
+	var solution [][]string
+	solution = append(solution, []string{"index"})
 
 	for i := 1; i < len(cities); i++ {
 		fmt.Println(len(cities))
 		if i == len(cities)-1 {
 			s := strconv.Itoa(int(cities[i].DistanceTo(cities[1])))
-			solution = append(solution, s)
+			solution = append(solution, []string{s})
 			continue
 		}
 		s := strconv.Itoa(int(cities[i].DistanceTo(cities[i+1])))
-		solution = append(solution, s)
+		solution = append(solution, []string{s})
 	}
 
 	printSolution(*in, solution)
